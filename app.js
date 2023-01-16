f2b19f8e6f58b499dad4991c0365bffbdfc161f1
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-01-16 22:53:33 +0100

    defining the first version of frontend and adding core js functions

---
+++ app.js
@@ -0,0 +1,59 @@
+function add(a,b){
+
+  return a+b
+
+}
+
+function substract(a,b){
+
+  return a-b
+}
+
+function multiply(a,b){
+
+  return a*b
+}
+
+function divide(a,b){
+
+  return a/b
+}
+
+function operate(operator, a,b){
+
+  if (operator=='+') {
+
+    return add(a,b)
+
+  } else {
+
+    if (operator=='-') {
+
+      return substract(a,b)
+
+    } else {
+
+      if (operator=='*') {
+
+        return multiply(a,b)
+
+      } else {
+
+        if (operator=='/') {
+
+          return divide(a,b)
+
+        } else {
+
+          return "No valid operator"
+
+        }
+
+      }
+
+    }
+
+
+  }
+
+}
