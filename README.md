e8da7715a370c7a3655e63d174c16814a580ca99
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-02-18 14:31:12 +0100

    Final version of the calculator app

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # calculator-project
+This calculator project is a simple web-based calculator built using HTML, CSS, and JavaScript. It is capable of performing basic arithmetic operations such as addition, subtraction, multiplication, and division.
+
+Installation
+
+To use this calculator, simply download the files from the repository and open the index.html file in your web browser.
+
+Usage
+
+The calculator provides a simple interface with a screen to display input and output. Users can input numbers and operators by clicking on the buttons provided. The calculator can perform the following operations:
+
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+To perform an operation, simply click the corresponding operator button after entering the first number, then enter the second number and click the equal (=) button. The result will be displayed on the screen.
+
+To clear the input, click the CLEAR button.
+
+Code
+
+The JavaScript code for this calculator project is included in the file app.js. The code defines four functions for performing the arithmetic operations and an additional function for handling the operation based on the operator input. The code also includes an event listener to listen for button clicks and update the calculator display accordingly.
+
+Contributing
+
+Contributions to this project are welcome. If you have any suggestions or improvements, please feel free to fork the repository and create a pull request.
+
+License
+
+This project is licensed under the MIT License. Please see the LICENSE file for details.
--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 function add(a,b){
+  a = parseFloat(a);
+  b = parseFloat(b);
 
-  return a+b
+  return a+b;
 
 }
 
 function substract(a,b){
-
-  return a-b
+  a = parseFloat(a);
+  b = parseFloat(b);
+  return a-b;
 }
 
 function multiply(a,b){
+  a = parseFloat(a);
+  b = parseFloat(b);
+  console.log(a,b);
 
-  return a*b
+  return a*b;
 }
 
 function divide(a,b){
-
-  return a/b
+  a = parseFloat(a);
+  b = parseFloat(b);
+  return a/b;
 }
 
 function operate(operator, a,b){
@@ -57,3 +64,60 @@ function operate(operator, a,b){
   }
 
 }
+
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach((element) => {
+
+  element.addEventListener('click', (event) => {
+    const display = document.querySelector('.screen');
+
+    if (event.currentTarget.innerText !== "=" && event.currentTarget.innerText!== "CLEAR") {
+      display.innerText += event.currentTarget.innerText;
+    }
+
+    if (event.currentTarget.innerText === "CLEAR") {
+      display.innerText = "";
+    }
+
+    if (event.currentTarget.innerText === "=") {
+      const operation = display.innerText;
+      let a = 0;
+      let b = 0;
+
+      if (operation.includes('+')) {
+        a = display.innerText.split('+')[0];
+        b = display.innerText.split('+')[1];
+
+        display.innerText = String(operate('+', a, b));
+      }
+
+      if (operation.substring(1).includes('-')) {
+        a = display.innerText.split('-')[0];
+        b = display.innerText.split('-')[1];
+        console.log(a,b);
+
+        display.innerText = String(operate('-', a, b));
+      }
+
+      if (operation.includes('*')) {
+        a = display.innerText.split('*')[0];
+        b = display.innerText.split('*')[1];
+        console.log(display.innerText);
+        console.log(a,b);
+
+        display.innerText = String(operate('*', a, b));
+
+      }
+
+      if (operation.includes('/')) {
+        a = display.innerText.split('/')[0];
+        b = display.innerText.split('/')[1];
+        display.innerText = String(operate('/', a, b));
+      }
+    }
+
+  })
+
+});
